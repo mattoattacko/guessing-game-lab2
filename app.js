94252002d095d724c081d2e0bade0c7086b88c89
Jamilah McWilliams <jamilahmcw@gmail.com>
2018-05-10 20:57:00 -0700

    Saving Lab 2 Changes

--- app.js
+++ app.js
@@ -5,20 +5,59 @@
 //console.log for useer imput
 //var user
 //use if else
-//use 'Hellow World' to test if JS file link works
 
 var user = prompt('Hi, What is your name?');
 
-alert('Hi ' + user);
+alert('Hi, ' + user + '!');
 
-var answer = prompt('Shall we play a game? Yes/No').toLowerCase();
+var question1 = prompt('Shall we play a game? Y/N').toLowerCase();
 
-console.log(answer);
+console.log(question1);
 
-if (answer === 'yes' || answer === 'y') {
+if (question1 === 'yes' || question1 === 'y') {
     //if answer is correct alert
-    var answer = prompt('Great! Let\'s play!');
+    var question1 = alert('Great! Let\'s play! Press Ok!');
 } else {
-    //if no alert
+    //if answer is no alert with movie quote
     alert('The only winning move is not to play.');
+}
+
+var question2 = prompt('Bonus Round! What movie was the previous question from?');
+
+console.log(question2);
+
+if (question2 === 'War Games' || question2 === 'war games') {
+    var question2 = alert('Congrats cult movie buff!') 
+} else {
+    alert('Ruh roh...wrong answer. Click OK and keep going to get to know more About Me!');
+}
+
+var question3 = prompt('Do you love to travel? Y/N').toLowerCase();
+
+console.log(question3);
+
+if (question3 === 'yes' || question3 === 'y') {
+    var question3 = alert('Yes! Will travel for FOOD and other cool stuff too.')
+} else {
+    alert('Are you crazy?! I LOVE to travel!');
+}
+
+var question4 = prompt('Do I love to cook? Y/N').toLowerCase();
+
+console.log(question4);
+
+if (question4 === 'yes' || question4 === 'y') {
+    var question4 = alert('I love to eat and I Love to cook. Fun fact...I am a culinary school grad.')
+} else {
+    alert('NO FOOD FOR YOU!')
+}
+
+var question5 = prompt('Do you love to workout?').toLowerCase();
+
+console.log(question5);
+
+if (question5 === 'yes' || question5 === 'y') {
+    var question5 = alert('I wouldn\'t say LOVE but I do when I have time.')
+} else {
+    alert('I do not LOVE it but I like to eat so...')
 }
